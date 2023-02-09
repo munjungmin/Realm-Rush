c1598c987045806b4ed7fa52e73fe455e0b657a3
munjungmin <answjdals77@gmail.com>
2023-02-09 22:29:10 +0900

    #2 Lerp() 적의 움직임 매끄럽게 하기

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -7,7 +7,8 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float waitTime = 1f;
+    [SerializeField] [Range(0f, 5f)] float speed = 1f;
+
     void Start()
     {
         StartCoroutine(FollowPath());
@@ -18,8 +19,21 @@ public class EnemyMover : MonoBehaviour
     {
         foreach(Waypoint waypoint in path)
         {
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(waitTime);
+            // Lerp : Linear Interpolation 시작과 종료 지점 간 움직임을 부드럽게 만들기
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            // 적이 가는 경로를 바라보도록 함
+            transform.LookAt(endPosition);  
+
+            while(travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();  // 프레임 끝까지 기다린 후 다시 코루틴 시작
+            }
+
         }
     }
     
