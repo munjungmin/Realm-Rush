b2e5ba27a42fd511d16d4c56067c50723346d8f3
munjungmin <answjdals77@gmail.com>
2023-02-10 18:42:00 +0900

    #4 적에게 데미지 주기

---
+++ Assets/EnemyHealth.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] int maxHitPoints = 5;
+    [SerializeField] int currentHitPoints = 0;
+
+    void Start()
+    {
+        currentHitPoints = maxHitPoints;
+    }
+
+    void OnParticleCollision(GameObject target) 
+    {
+        ProcessHit();
+    }
+
+    void ProcessHit()
+    {
+        currentHitPoints--;
+
+        if(currentHitPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
