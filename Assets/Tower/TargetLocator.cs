41d5ed263e90ca03566954003bf6a95c984fd352
munjungmin <answjdals77@gmail.com>
2023-02-15 01:25:09 +0900

    #4 가장 가까운 적 겨냥하기

--- Assets/Tower/TargetLocator.cs
+++ Assets/Tower/TargetLocator.cs
@@ -5,22 +5,57 @@ using UnityEngine;
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform weapon;
+    [SerializeField] ParticleSystem projectileParticles;
+    [SerializeField] float range = 15f;
     Transform target;
-
-    void Start()
-    {
-        // 씬 전체에 있는 첫 번째 EnemyMover를 탐색
-        target = FindObjectOfType<EnemyMover>().transform;
-        
-    }
 
     void Update()
     {
+        FindClosestTarget();  // 게임 규모가 큰 경우, 적이 죽거나 범위를 벗어나는 등 특정 조건 하에서만 실행하면 성능 부담 낮아짐
         AimWeapon();
+    }
+
+    void FindClosestTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach(Enemy enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+            
+            if(targetDistance < maxDistance)
+            {
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+
+        target = closestTarget; 
     }
 
     void AimWeapon()
     {
+        float targetDistance = Vector3.Distance(transform.position, target.position);
+
+        if (targetDistance < range)
+        {
+            Attack(true);
+        }
+        else
+        {
+            Attack(false);
+        }
+
         weapon.LookAt(target);   
+
+        
+    }
+
+    void Attack(bool isActive)
+    {
+        var emissionModule = projectileParticles.emission;
+        emissionModule.enabled = isActive;
     }
 }
