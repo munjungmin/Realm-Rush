425fe040f31326f84ae0cf0df8e8d57b3eb67017
munjungmin <answjdals77@gmail.com>
2023-02-15 02:26:12 +0900

    #10 currency UI text

--- Assets/Bank/Bank.cs
+++ Assets/Bank/Bank.cs
@@ -2,28 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
     [SerializeField] int startingBalance = 150;
     [SerializeField] int currentBalance;
+    [SerializeField] TextMeshProUGUI displayBalance;
 
     public int CurrentBalance {get { return currentBalance; } }
 
     void Awake() 
     {
         currentBalance = startingBalance;
+        UpdateDisplay();
     }
 
     public void Deposit(int amount)
     {
         currentBalance += Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
-
+        UpdateDisplay();
+        
         if (currentBalance < 0)
         {
             // Lose the game;
@@ -31,6 +36,10 @@ public class Bank : MonoBehaviour
         }
     }
     
+    void UpdateDisplay()
+    {
+        displayBalance.text = "Gold: " + currentBalance;
+    }
     void ReloadScene()
     {
         Scene currentScene = SceneManager.GetActiveScene();
